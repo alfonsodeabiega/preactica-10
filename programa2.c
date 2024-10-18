a18f17cff31a62872dedf462c3d020e692a02647
alfonsodeabiega <alfonso.deabiega@gmail.com>
2024-10-18 13:59:24 -0600

    programa2.c

---
+++ programa2.c
@@ -0,0 +1,24 @@
+/*Alfonso Francisco De Abiega L'Eglisse
+matricez, practica 10*/
+  
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int i = 0, j = 0;
+   
+    printf("Imprimir matriz\n");
+    while(i < 3)
+    {
+        while (j < 3)
+        {
+            printf("%d | ", arr[i][j]);
+            j++;
+        }
+        printf("\n");
+        j = 0;
+        i++;
+    }
+    return 0;
+}
